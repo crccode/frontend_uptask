61fedf62ec8b7ae1c1678de882a8f61c8cd0bec6
crccode <54750603+crccode@users.noreply.github.com>
2023-08-27 11:11:31 -0400

    CREAMOS STATE PARA CADA CAMPO - VALIDACION FORMULARIO- CONEXION API AXIOS

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,14 +1,89 @@
 import React from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Alerta from '../components/Alerta'
+import axios from "axios";
+
 const Registrar = () => {
+  // DEFINIENDO STATE PARA CADA CAMPO
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repetirPassword, setRepetirPassword] = useState("");
+
+  const [ alerta, setAlerta ] = useState({}); // ES UN OBJETO
+
+  // VALIDACION DEL FORMULARIO HANDLECUBMIT
+  const handleSubmit = async (e) => {
+    // PREVIENE LA ACCION POR DEFECTO DE ENVIAR EL FORMULARIO
+    e.preventDefault();
+    // VALIDAMOS QUE TODOS LOS CAMPOS TENGAN ALGO
+    if ([nombre, email, password, repetirPassword].includes("")) {
+      setAlerta({
+        msg: "Todos los campos son obligatorios",
+        error: true,
+      });
+      return; //PARA QUE NO SIGA EJECUTANDO EL CODIGO
+    }
+    // VALIDACION DE PASSWORD
+    if (password !== repetirPassword) {
+      setAlerta({
+        msg: "Los password no son iguales",
+        error: true,
+      });
+      return;
+    }
+    // VALIDACION LA LONGITUD DEL PASSWORD
+    if (password.length < 6) {
+      setAlerta({
+        msg: "El Password es muy corto, agrega minimo 6 caracteres",
+        error: true,
+      });
+      return;
+    }
+
+    setAlerta({});
+
+    // Crear el usuario en la API
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/usuarios`, {
+        nombre,
+        email,
+        password,
+      });
+
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+
+      setNombre("");
+      setEmail("");
+      setPassword("");
+      setRepetirPassword("");
+    } catch (error) { //EN EL CASO DE QUE HAYA ERROR LEEMOS LOS MSG DEL SERVER
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
+  // EXTRAEMOS EL MENSAJE DE LA ALERTA
+  const { msg } = alerta
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Crea tu Cuenta y Administra tus {""}
         <span className="text-slate-700">proyectos</span>
       </h1>
-
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {/* SI EL MENSAJE TIENE ALGO LO MUESTRA  */}
+      { msg && <Alerta alerta={alerta} /> }
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             className="uppercase text-gray-600 block text-xl font-bold"
@@ -21,6 +96,8 @@ const Registrar = () => {
             type="text"
             placeholder="Tu Nombre"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
 
@@ -36,6 +113,8 @@ const Registrar = () => {
             type="email"
             placeholder="Email de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -50,6 +129,8 @@ const Registrar = () => {
             type="password"
             placeholder="Password de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
@@ -65,6 +146,8 @@ const Registrar = () => {
             type="password"
             placeholder="Repetir tu Password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={repetirPassword}
+            onChange={(e) => setRepetirPassword(e.target.value)}
           />
         </div>
         {/* BOTON  */}
