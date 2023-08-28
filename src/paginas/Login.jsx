07e9f31a3af6ea781f13d31b1946731341f609fd
crccode <54750603+crccode@users.noreply.github.com>
2023-08-27 21:46:08 -0400

    Pantalla LOGIN inicio de secion

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,71 +1,108 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import { Link,  useNavigate } from 'react-router-dom'
+import Alerta from '../components/Alerta'
+import clienteAxios from '../config/clienteAxios'
+
 const Login = () => {
+  // CREAMOS EL ESTATE 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [alerta, setAlerta] = useState({})
+
+  const navigate = useNavigate()
+
+  // INTERACTUAMOS CON LA API
+  const handleSubmit = async e => {
+      e.preventDefault();
+
+      if([email, password].includes('')) {
+          setAlerta({
+              msg: 'Todos los campos son obligatorios',
+              error: true
+          });
+          return
+      }
+      // VALIDAMOS EL PASSWORD
+      try {
+          const { data } = await clienteAxios.post('/usuarios/login', { email, password})
+          setAlerta({})
+          // ALMACENAMOS EL TOKEN EN LOCALSTORAGE
+          localStorage.setItem('token', data.token)
+
+      } catch (error) {
+          setAlerta({
+              msg: error.response.data.msg,
+              error: true
+          })
+      }
+
+  }
+
+  const { msg } = alerta
+
   return (
     <>
-      <h1 className="text-sky-600 font-black text-6xl capitalize">
-        Inicia sesión y administra tus {""}
-        <span className="text-slate-700">proyectos</span>
-      </h1>
+        <h1 className="text-sky-600 font-black text-6xl capitalize">Inicia sesión y administra tus {''}
+            <span className="text-slate-700">proyectos</span>
+        </h1>
 
-      <form
-        className="my-10 bg-white shadow rounded-lg p-10"
-      >
-      {/* PRIMER ELEMENTO  */}
-        <div className="my-5">
-          <label
-            className="uppercase text-gray-600 block text-xl font-bold"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Email de Registro"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-          />
-        </div>
-        {/* SEGUNDO ELEMENTO  */}
-        <div className="my-5">
-          <label
-            className="uppercase text-gray-600 block text-xl font-bold"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Password de Registro"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-          />
-        </div>
-        {/* BOTON  */}
-        <input
-          type="submit"
-          value="Iniciar Sesión"
-          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
-        />
-      </form>
-      {/* NAVEGACION ROUTERDOM  */}
-      {/* lg:justify-between COLOCA LOS ELEMENTOS UNO A LA DERECHA E IZQUIERDA  */}
-      <nav className="lg:flex lg:justify-between">
-        <Link
-          className="block text-center my-5 text-slate-500 uppercase text-sm"
-          to="/registrar"
+        {msg && <Alerta alerta={alerta } />}
+    
+        <form 
+            className="my-10 bg-white shadow rounded-lg p-10"
+            onSubmit={handleSubmit}
         >
-          ¿No tienes una cuenta? Regístrate
-        </Link>
+            <div className="my-5">
+                <label 
+                    className="uppercase text-gray-600 block text-xl font-bold"
+                    htmlFor="email"
+                >Email</label>
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="Email de Registro"
+                    className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                    value={email}
+                    onChange={ e => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="my-5">
+                <label 
+                    className="uppercase text-gray-600 block text-xl font-bold"
+                    htmlFor="password"
+                >Password</label>
+                <input
+                    id="password"
+                    type="password"
+                    placeholder="Password de Registro"
+                    className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                    value={password}
+                    onChange={ e => setPassword(e.target.value)}
+                />
+            </div>
 
-        <Link
-          className="block text-center my-5 text-slate-500 uppercase text-sm"
-          to="/olvide-password"
-        >
-          Olvide Mi Password
-        </Link>
-      </nav>
+            <input 
+                type="submit"
+                value="Iniciar Sesión"
+                className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+            />
+            
+        </form>
+
+        <nav className="lg:flex lg:justify-between">
+            <Link 
+                className='block text-center my-5 text-slate-500 uppercase text-sm'
+                to="/registrar"
+            >¿No tienes una cuenta? Regístrate</Link>
+
+            <Link 
+                className='block text-center my-5 text-slate-500 uppercase text-sm'
+                to="/olvide-password"
+            >Olvide Mi Password</Link>
+        </nav>
+    
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
