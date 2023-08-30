222bc7c2795610cced87b946ab9ef31549c66732
crccode <54750603+crccode@users.noreply.github.com>
2023-08-30 14:33:26 -0400

    CONTEXT PROYECTO - HOOKS

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import NuevoPassword from "./paginas/NuevoPassword";
 import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 
 import {AuthProvider} from "./context/AuthProvider"
+import {ProyectosProvider} from "./context/ProyectosProvider"
 import Proyectos from "./paginas/Proyectos";
 import RutaProtegida from "./layouts/RutaProtegida";
 import NuevoProyecto from "./paginas/NuevoProyecto";
@@ -14,29 +15,32 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-      {/* GRUPO DE RUTAS  */}
-        <Routes>
-        {/* RUTA PRINCIPAL  */}
-          {/* AREA PUBLICA  */}
-          <Route path="/" element={<AuthLayout />}>
-          {/* LO QUE SE VA A EJECUTAR CUANDO SE CARGUE / PAGINA HOME  */}
-            <Route index element={<Login />} />
-            <Route path="registrar" element={<Registrar />} />
-            <Route path="olvide-password" element={<OlvidePassword />} />
-            <Route path="olvide-password/:token" element={<NuevoPassword />} />
-            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-          </Route>
-          {/* RUTA PROTEGIDA  */}
-          <Route path="/proyectos" element={<RutaProtegida />}>
-            <Route index element={<Proyectos />} />
-     
-            <Route path="crear-proyecto" element={<NuevoProyecto />} />
-            {/* <Route path="nuevo-colaborador/:id" element={<NuevoColaborador />} />
-            <Route path=":id" element={<Proyecto />} />
-            <Route path="editar/:id" element={<EditarProyecto />} /> */}
-          </Route>
-        </Routes>
+      <ProyectosProvider>
+        {/* GRUPO DE RUTAS  */}
+          <Routes>
+          {/* RUTA PRINCIPAL  */}
+            {/* AREA PUBLICA  */}
+            <Route path="/" element={<AuthLayout />}>
+            {/* LO QUE SE VA A EJECUTAR CUANDO SE CARGUE / PAGINA HOME  */}
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route path="olvide-password/:token" element={<NuevoPassword />} />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
+            {/* RUTA PROTEGIDA  */}
+            <Route path="/proyectos" element={<RutaProtegida />}>
+              <Route index element={<Proyectos />} />
+      
+              <Route path="crear-proyecto" element={<NuevoProyecto />} />
+              {/* <Route path="nuevo-colaborador/:id" element={<NuevoColaborador />} />
+              <Route path=":id" element={<Proyecto />} />
+              <Route path="editar/:id" element={<EditarProyecto />} /> */}
+            </Route>
+          </Routes>
+        </ProyectosProvider>
       </AuthProvider>
+        
     </BrowserRouter>
   );
 }
