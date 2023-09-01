4ed2369d915cd9884c8e8dd0308aefb10ace0740
crccode <54750603+crccode@users.noreply.github.com>
2023-08-31 23:35:31 -0400

    CREAMOS PROYECTOS - FORMULARIO PROYECTO - PROYECTO PROVIDER

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -1,14 +1,60 @@
 import { useState, useEffect, createContext } from 'react'
 import clienteAxios from '../config/clienteAxios'
+import { useNavigate } from 'react-router-dom'
 
 const ProyectosContext = createContext();
 
 const ProyectosProvider = ({children}) => {
+    // HOKS PARA NAVEGAR
+    const navigate = useNavigate();
 
+    const [proyectos, setProyectos] = useState([]);
+    const [alerta, setAlerta] = useState({});
+    const mostrarAlerta = alerta => {
+        setAlerta(alerta)
+        // DESPUES DE 5 SEG SE REINICIA HACIA UN OBJETO VACIO
+        setTimeout(() => {
+            setAlerta({})
+        }, 5000);
+    }
+    // ESTA FUNCION VA INTERACTURAR CON LA API
+    const submitProyecto = async proyecto => {
+        // AGREGAMOS EL PROYECTO PERO NECESITAMOS EL TOKEN PARA HACERLO, LO USAMOS DEL LOCALSTORAGE
+        try {
+            const token = localStorage.getItem('token')
+            if (!token) return 
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            // data ES LA RESUPUESTA DE AXIOS
+            // proyecto ESTO VIENE DEL FORMULARIO
+            const { data } = await clienteAxios.post('/proyectos', proyecto, config)
+
+            setAlerta({
+                msg: 'Proyecto creado exitosamente',
+                error: false
+            })
+            // DESPUIES DE 3 SEG LO REINICIARE
+            setTimeout(() => {
+                setAlerta({})
+                navigate('/proyectos')
+            }, 3000);
+        }catch (error) {
+            console.log(error)
+        }
+    }
+    
     return (
         <ProyectosContext.Provider
             value={{
-              
+              proyectos,
+              mostrarAlerta,
+              alerta,
+              submitProyecto
             }}
         >{children}
         </ProyectosContext.Provider>
--- src/paginas/Proyectos.jsx
+++ src/paginas/Proyectos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Proyectos = () => {
+  
   return (
     <div>
       PROYECTOS     
